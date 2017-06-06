e43ca14b66d097b2ea8c2c56af4002181d934182
mikemajesty <mikemajesty2011@gmail.com>
2017-06-06 14:11:26 -0300

    Add style configuration

--- StormReport/Program.cs
+++ StormReport/Program.cs
@@ -4,13 +4,15 @@ using System.Web.UI.WebControls;
 using System.Web.Mvc;
 using System.Drawing;
 using System.Web.UI;
+using System;
+using System.Data;
 
 namespace StormReport
 {
     public class Program : Controller
     {
 
-        public void teste<T>(List<T> list, )
+        public void teste<T>(IList<T> list, List<ColumnConfig> config)
         {
             var excel = new System.Data.DataTable("teste");
             excel.Columns.Add("CPF/CNPJ", typeof(double));
@@ -18,15 +20,15 @@ namespace StormReport
             excel.Columns.Add("Descrição", typeof(string));
             excel.Columns.Add("Justificação", typeof(string));
 
-            /*for (int row = 0; row < response.Result.Count; row++)
+            for (int row = 0; row < list.Count; row++)
             {
                 DataRow newrow = excel.NewRow();
-                newrow[excel.Columns["CPF/CNPJ"]] = response.Result[row].CnpjCpf;
-                newrow[excel.Columns["Data Cadastro"]] = response.Result[row].DataCadastro;
-                newrow[excel.Columns["Descrição"]] = response.Result[row].Descricao;
-                newrow[excel.Columns["Justificação"]] = response.Result[row].Justificacao;
+                newrow[excel.Columns["CPF/CNPJ"]] = list[row].CnpjCpf;
+                newrow[excel.Columns["Data Cadastro"]] = list[row].DataCadastro;
+                newrow[excel.Columns["Descrição"]] = list[row].Descricao;
+                newrow[excel.Columns["Justificação"]] = list[row].Justificacao;
                 excel.Rows.Add(newrow);
-            }*/
+            }
 
             var grid = new GridView();
             grid.DataSource = excel;
@@ -45,6 +47,13 @@ namespace StormReport
             Response.Output.Write(sw.ToString());
             Response.Flush();
             Response.End();
+        }
+
+        public class ColumnConfig
+        {
+            public string Name { get; set; }
+            public Type Type { get; set; }
+            public List<TableItemStyle> Styles { get; set; }
         }
 
 
