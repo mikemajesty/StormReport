957ba2501bd7188bb99afc601c31fd3254064b20
mike.peitudo <mikemajesty2011@gmail.com>
2017-06-13 17:23:38 -0300

    Add more annotations

---
+++ StormReport/Annotations/ExportableColumnGroupAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StormReport.Test
+{
+    public class ExportableColumnGroupAttribute : Attribute
+    {
+        public string Description { get; set; }
+        public ExportableColumnGroupAttribute(string description)
+        {
+            this.Description = description;
+        }
+    }
+}
--- StormReport/Model/MockEmployee.cs
+++ StormReport/Model/MockEmployee.cs
@@ -17,21 +17,25 @@ namespace StormReport.Test
         [ExportableColumnHeaderStyle("text-align: center;", "color: red;", "font-size: 17px;")]
         [ExportableColumnContentStyle("text-align: center;", "color: gray;", "font-size: 17px;")]
         [ExportableAddtionalText("R$ ", AdditionalTextEnum.LEFT)]
+        [ExportableColumnGroup("Cliente")]
         public string Nome { get; set; }
 
         [ExportableColumnHeaderName("Idade do Cliente")]
         [ExportableColumnHeaderStyle("text-align: center;", "color: red;", "font-size: 17px;")]
         [ExportableColumnContentStyle("text-align: center;", "color: blue;", @"font-size: 17px; mso-number-format: '0\.000'")]
+        [ExportableColumnGroup("Cliente")]
         public int Idade { get; set; }
 
         [ExportableColumnHeaderName("Cidade do Cliente")]
         [ExportableColumnHeaderStyle("text-align: center;", "color: red;", "font-size: 17px;")]
         [ExportableColumnContentStyle("text-align: center;", "color: gray;", "font-size: 17px;")]
+        [ExportableColumnGroup("Cliente")]
         public string Cidade { get; set; }
 
         [ExportableColumnHeaderName("Estado do Cliente")]
         [ExportableColumnHeaderStyle("text-align: center;", "color: red;", "font-size: 17px;")]
         [ExportableColumnContentStyle("text-align: center;", "color: blue;", "font-size: 17px;")]
+        [ExportableColumnGroup("Cliente")]
         public string Estado { get; set; }
 
         public void Add(ClassFinalTest e)
